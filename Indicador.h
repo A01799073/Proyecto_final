26f9a6d15ee539d93f2d72662106c1200e07bcf0
A01799073 <A01799073@tec.mx>
2023-06-06 23:17:26 -0600

    Clase indicador
    Se creo la clase, se edito la estructura de la clase

---
+++ Indicador.h
@@ -0,0 +1,19 @@
+/*
+    *Autor: Kenia Esmeralda Ramos Javier.
+    *Representa el sistema de alerta.
+    *Declaración de la clase Indicador
+ */
+
+#ifndef INDICADOR_H
+#define INDICADOR_H
+class Indicador
+{
+private:
+    int velocidadMaxima=160;
+
+public:
+    Indicador();
+    Indicador(int velocidad);
+
+};
+#endif
