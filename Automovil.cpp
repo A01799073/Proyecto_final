db48efe429d4b28e5b0053b3a3f66422cb716f86
A01799073 <A01799073@tec.mx>
2023-06-11 14:48:29 -0600

    Create Automovil.cpp

---
+++ Automovil.cpp
@@ -0,0 +1,54 @@
+/*
+    Autor:Kenia Esmeralda Ramos Javier.
+    Implementación de la clase Automovil.
+*/
+
+#include "Automovil.h"
+using namespace std;
+bool Auto::encender()
+{
+    return false;
+}
+
+bool Auto::apagar()
+{
+    return false;
+}
+
+bool Auto::acelerar()
+{
+    return false;
+}
+
+bool Auto::frenar()
+{
+    return false;
+}
+
+bool Auto::prenderLuces()
+{
+    return false;
+}
+
+bool Auto::apagarLuces()
+{
+    return false;
+}
+
+int get.Auto::VelocidadActual()
+{
+    return 0;
+}
+
+int Auto::getVelocidadActual()
+{
+    return 0;
+}
+
+void Auto::setVelocidadActual()
+{
+}
+
+void Auto::imprimir()
+{
+}
