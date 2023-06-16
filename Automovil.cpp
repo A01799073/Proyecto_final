99546425360938b0f73bd2f3fa813178b76966e9
A01799073 <A01799073@tec.mx>
2023-06-15 21:46:41 -0600

    edición de la clase auto
    Para esta clase se volvio a plantear como hacer el codigo, porque había variables que no eran necesarias y por lo tanto también se modificará el diagrama más adelante

--- Automovil.cpp
+++ Automovil.cpp
@@ -4,7 +4,95 @@
 */
 
 #include "Automovil.h"
-#include "Luces.h"
-#include <iostream>
-using namespace std;
 
+Automovil::Auto()
+{
+    bool estadoAuto=false;
+    bool estadoLuces=false;
+    int velocidadInial=0;
+    int velocidadActual=0;
+    int numLlantas=4;
+}
+
+bool Automovil::encender()
+{
+    if (estadoAuto=true)
+    {
+        cout << "El ya auto se esta encedido" << endl;
+    }
+
+    else
+    {
+        estadoAuto = !estadoAuto;
+        cout << "Ahora el auto esta prendido" << endl;
+        return estadoAuto;
+    }
+}
+
+bool Automovil::apagar()
+{
+    if (estadoAuto=false)
+    {
+        cout << "El auto ya esta apagado" << endl;
+    }
+    else
+    {
+        estadoAuto = !estadoAuto;
+        cout << "Ahora el auto esta apagado" << endl;
+        return estadoAuto;
+    }
+}
+
+int Automovil::getVelocidadActual()
+{
+    return velocidadActual;
+}
+
+void Automovil::setVelocidadActual()
+{
+
+}
+
+
+
+bool Automovil::acelerar()
+{
+        if (estado==true && velocidadActual< 230)
+    {
+        velocidadActual+=15;
+        return velocidadActual;
+    }
+    return false;
+}
+bool Automovil::frenar()
+{
+    if (estado==true)
+    {
+        velocidadActual-=25;
+        if (velocidadActual<0)
+        {
+            velocidadActual=0;
+            cout<<"La velocidad ha llegado a"<<velocidadActual<<endl;
+        }
+    }
+    return false;
+}
+
+bool Automovil::prenderLuces()
+{
+    return true;
+}
+
+bool Automovil::apagarLuces()
+{
+    return false;
+}
+
+void Automovil::aviso()
+{
+    if (velocidadIncial>160)
+    {
+        cout<<"¡PELIGRO!"
+    }
+    
+}
