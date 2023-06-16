0e4af54109c9afb4f487b75eece8d02235a4512a
A01799073 <A01799073@tec.mx>
2023-06-15 20:10:15 -0600

    Clase Luces
    Se declara la clase luces respecto a nuestro diagrama

---
+++ Luces.h
@@ -0,0 +1,19 @@
+/*
+ *Autor: Kenia Esmeralda Ramos Javier.
+ *Declaración de la clase Luces.
+ */
+
+#ifndef LUCES_H
+#define LUCES_H
+
+class Luces
+{
+private:
+    bool estado;
+public:
+    Luces();
+
+    bool getEstados();
+    bool setEstado();
+};
+#endif
