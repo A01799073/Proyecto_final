54578cca2568c585852efa4f13f99d08550923fc
A01799073 <A01799073@tec.mx>
2023-06-15 22:21:43 -0600

    Update menu
    Se modifico el menu, ya que no me agrado y no era funcional

--- menu.cpp
+++ menu.cpp
@@ -6,73 +6,12 @@
     Descripción: Programa principal  de la situación problema
 */
 
-/* ---------PROGRAMA EN CONSTRUCCIÓN-----------*/
-
 #include <iostream>
-/*
-#include  --> archivos .h
-*/
+#include "Automovil.h"
+#include "Tanque.h"
 using namespace std;
 
 // programa principal
 int main()
 {
-    int respuesta = 1;
-    while (int respuesta == 1;)
-    {
-        cout << "¡¡¡¡BIENVENIDO!!!" << endl;
-        cout << "El menu de busqueda es:" << endl;
-        cout << "1.-Prender/Apagar el auto..................." << endl;
-        cout << "2.-Acelerar................................." << endl;
-        cout << "3.-Frenar..................................." << endl;
-        cout << "4.-Prender las luces........................" << endl;
-        cout << "5.-Apagar las luces........................." << endl;
-        cout << "6.-Cargar gasolina.........................." << endl;
-        cout << "7.-Indicador de la presion de llantas......." << endl;
-        cout << "8.-Salir...................................." << endl;
-
-        while (num = cin.get() != 1 && num != 2 && num != 3 && num != 4 && num != 5 && num != 6 && num != 7 && pedido num != 8)
-        {
-            cout << "Opcion no disponible dentro del menu establecido" << endl;
-        }
-
-        if (num == 1)
-        {
-            /* code */
-        }
-        else if (num == 2)
-        {
-            /* code */
-        }
-        else if (num == 3)
-        {
-            /* code */
-        }
-        else if (num == 4)
-        {
-            /* code */
-        }
-        else if (num == 5)
-        {
-            /* code */
-        }
-        else if (num == 6)
-        {
-            /* code */
-        }
-        else if (num == 7)
-        {
-            /* code */
-        }
-        else if (num == 8)
-        {
-            cout << "Hasta la proxima" << endl;
-        }
-
-        while (registro = cin.get() != 1 && registro != 2)
-        {
-            cout << "\n¿Desea ver otra cosa del menú: 1.)Sí o 2.)No: ";
-            cout << "Debe elegir 1 o 2." << endl;
-        }
-    }
 }
